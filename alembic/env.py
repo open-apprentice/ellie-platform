b0d6139add076e0864b49d06d48cb86183f3438f
davidkartuzinski <david@kaidawei.me>
2021-08-08 01:56:51 +0200

    Course table added to database with alembic. Migration.

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# from models.users import User
+from models.courses import Course
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,7 +29,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [Course.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
